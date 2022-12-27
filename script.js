d0f9462a18caa5b836daa58a7e766f4563ca93b3
Thoriq Farras <thoriqibrahimfarras@gmail.com>
2022-12-27 10:01:01 +0700

    createed playRound()

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let numChoice = Math.floor(Math.random() * 3) + 1;
+    const numChoice = Math.floor(Math.random() * 3) + 1;
     let wordChoice;
     switch(numChoice) {
         case 1:
@@ -15,4 +15,32 @@ function getComputerChoice() {
             break;
     }
     return wordChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let outcome;
+    let playerChoice = playerSelection.toLowerCase();
+    let computerChoice = computerSelection;
+    if (playerChoice == computerChoice) {
+        outcome = 'draw';
+    } else if (playerChoice == "rock") {
+        if (computerChoice == 'paper') {
+            outcome = 'lose';
+        } else if (computerChoice == 'scissors') {
+            outcome = 'win';
+        }
+    } else if (playerChoice == "paper") {
+        if (computerChoice == "rock") {
+            outcome = 'win';
+        } else if (computerChoice == 'scissors') {
+            outcome = 'lose';
+        }
+    } else if (playerChoice == "scissors") {
+        if (computerChoice == "rock") {
+            outcome = 'lose';
+        } else if (computerChoice == 'paper') {
+            outcome = 'win';
+        }
+    }
+    return outcome;
 }
